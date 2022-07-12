938e9c8b578accfd8db358beadb3832227000c13
subodhstha <shresthasubodh73@gmail.com>
2022-07-12 18:06:23 +0545

    node

--- tutorial/10-fs-sync.js
+++ tutorial/10-fs-sync.js
@@ -3,7 +3,7 @@ const {readFileSync, writeFileSync} = require("fs")
 
 console.log("start")
 
-const first = readFileSync("./content/first.txt", "utf8")
+const first = readFileSync("../first.txt", "utf8")
 const second = readFileSync("./content/second.txt", "utf8")
 
 writeFileSync(
---
+++ tutorial/apps.js
@@ -0,0 +1,13 @@
+const {readFile} = require("fs");
+
+console.log("started a first task");
+
+readFile("./content/first.txt","utf8",(err, result)=>{
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log(result);
+    console.log("complete first task");
+})
+console.log("starting next task");
