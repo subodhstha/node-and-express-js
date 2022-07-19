d1777ec37bad521760cd5b67ac158625614012c8
subodhstha <shresthasubodh73@gmail.com>
2022-07-19 22:45:13 +0545

    express

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -5,9 +5,40 @@ let {people} = require("./data.js")
 // static assets
 app.use(express.static("2-express-tutorial/methods-public"))
 
+// parse form data
+app.use(express.urlencoded({extended: false}))
+// parse json
+app.use(express.json())
+
 app.get("/api/people",(req,res)=>{
     res.status(200).json({success:true,data:people})
 })
+
+
+app.post("/api/people",(req, res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success:false,meg:"please provide name value"})
+    }
+    res.status(201).send({success:true,person:name})
+})
+
+app.post("/api/postman/people",(req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success:false,meg:"please provide name value"})
+    }
+    res.status(201).send({success:true,person:name})
+})
+
+app.post("/login",(req, res)=>{
+    const {name} = req.body
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send("Please Provide Credentials")
+})
+
 app.listen(5000, ()=>{
     console.log(("Server is listening on port 5000"));
 })
