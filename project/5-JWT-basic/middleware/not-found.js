eacc6ca4143fdfda07223ff5bf17c32087d15459
Subodh Shrestha <shresthasubodh73@gmail.com>
2022-08-03 12:34:30 +0545

    project

--- project/5-JWT-basic/middleware/not-found.js
+++ project/5-JWT-basic/middleware/not-found.js
@@ -1,3 +1,3 @@
-const notFound = (req, res) => res.status(404).status(404).send("Route does not exit")
+const notFound = (req, res) => res.status(404).send('Route does not exist')
 
 module.exports = notFound
