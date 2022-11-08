8a5dc4488cc3c70c3c781cd6f6fb45dee84309d3
Subodh Shrestha <shresthasubodh73@gmail.com>
2022-11-08 20:14:50 +0545

    jwt

--- project/5-JWT-basic/app.js
+++ project/5-JWT-basic/app.js
@@ -4,12 +4,15 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const mainRouter = require("./routes/main");
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
+
+app.use("/api/v1",mainRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -18,9 +21,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    app.listen(port, console.log(`Server is listening on port ${port}...`))
   } catch (error) {
     console.log(error);
   }
