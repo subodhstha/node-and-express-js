3b34ebb9b16401387b9413ed245638078cea8b28
Subodh Shrestha <shresthasubodh73@gmail.com>
2022-11-17 10:04:15 +0545

    job api

--- project/6-jobs-api/controllers/jobs.js
+++ project/6-jobs-api/controllers/jobs.js
@@ -1,13 +1,19 @@
-const getAllJobs = async(req, res) =>{
-    res.send("Get all jobs")
-}
+const Job = require("../models/Job")
+const {StatusCodes}= require("http-status-codes")
+const {BadRequestError, NotFoundError} = require("../errors") 
 
+const getAllJobs = async(req, res) =>{
+    const jobs  = await Job.find({createdBy: req.user.userId}).sort("createdAt")
+    res.status(StatusCodes.OK).json({jobs, count: jobs.length})
+}
 const getJob = async(req, res) =>{
     res.send("Get job")
 }
 
 const createJob = async(req, res) =>{
-    res.json(req.user)
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
 }
 
 const updateJob = async(req, res) =>{
