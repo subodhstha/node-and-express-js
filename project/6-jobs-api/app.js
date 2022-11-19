7527719429debb7f4999533d064d9753e22e4674
Subodh Shrestha <shresthasubodh73@gmail.com>
2022-11-19 12:14:36 +0545

    job api

--- project/6-jobs-api/app.js
+++ project/6-jobs-api/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security package
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimit = require("express-rate-limit")
+
 const express = require('express');
 const app = express();
 
@@ -14,8 +21,17 @@ const jobsRouter = require("./routes/jobs")
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set("trust proxy", 1)
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100
+}))
+
 app.use(express.json());
 // extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use("/api/v1/auth", authRouter)
