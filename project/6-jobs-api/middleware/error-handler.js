81313fdc1bede39d3b6ea7c47211007bd4b11799
Subodh Shrestha <shresthasubodh73@gmail.com>
2022-11-19 10:54:59 +0545

    job api

--- project/6-jobs-api/middleware/error-handler.js
+++ project/6-jobs-api/middleware/error-handler.js
@@ -1,10 +1,26 @@
-const { CustomAPIError } = require('../errors')
+// const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong try again later"
+
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+  if (err.name === "ValidationError") {
+    // console.log(Object.values(err.errors));
+    customError.msg = Object.values(err.errors).map((item)=>item.message).join(",")
+    customError.statusCode = 400
+  }
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value for ${Object.keys(err.keyValue)} field, please choose another value`
+    customError.statusCode = 400
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({msg: customError.msg})
 }
 
 module.exports = errorHandlerMiddleware
--- project/6-jobs-api/models/User.js
+++ project/6-jobs-api/models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
     },
     email:{
         type:String,
-        required:[true, "Please provide name"],
+        required:[true, "Please provide email"],
         match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ,
         "Please provide valid email"
         ],
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema({
     },
     password:{
         type: String,
-        require: [true, "Please provide password"],
+        required: [true, "Please provide password"],
         minlength: 6,
     },
 })
